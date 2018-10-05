73696af5ab5e043182605d193b423457f0c13465
smallsun <xxx.com>
2018-10-05 15:36:58 +0800

    involve nunjucks

--- controller/home.js
+++ controller/home.js
@@ -14,16 +14,9 @@ module.exports = {
     ctx.response.body = '<h1>HOME page /:id/:name</h1>'
   },
   login: async (ctx, next) => {
-    ctx.response.body =
-      `
-      <form action="/user/register" method="post">
-        <input name="name" type="text" placeholder="请输入用户名：ikcamp"/> 
-        <br/>
-        <input name="password" type="text" placeholder="请输入密码：123456"/>
-        <br/> 
-        <button>GoGoGo</button>
-      </form>
-    `
+    await ctx.render('home/login', {
+      btnName: 'Register Now'
+    });
   },
   register: async (ctx, next) => {
     let {
